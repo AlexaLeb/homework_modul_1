4229a15d63b7ab3e3605d0e6266a4927e8ef1f04
AlexaLeb <118658187+AlexaLeb@users.noreply.github.com>
2025-04-22 10:32:04 +0300

    тесты рабочие

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from database.database import get_settings
+from database.database import init_db
+
+if __name__ == "__main__":
+
+    settings = get_settings()
+    print(settings.DB_HOST)
+    print(settings.DB_NAME)
+
+    init_db()
+    print('init db success')
+
+
